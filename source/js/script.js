4652f288e27576d1b2bfe1f243b415bbe4e4e52c
profiphp <profiphp@gmail.com>
2020-01-17 16:11:50 +0400

    Переделал верхний блок с меню

--- source/js/script.js
+++ source/js/script.js
@@ -1,23 +1,20 @@
-const navToggle = document.querySelector('.page-header__toggle');
-const mainNav = document.querySelector('.main-nav');
-const headerLogo = document.querySelector('.page-header__logo-wrapper');
-const offerSlogan = document.querySelector('.offer__slogan');
+var pageHeader = document.querySelector('.page-header');
+var navToggle = document.querySelector('.page-header__toggle');
 
-navToggle.classList.remove('page-header__toggle--nojs');
-mainNav.classList.remove('main-nav--nojs');
-headerLogo.classList.remove('page-header__logo-wrapper--nojs');
+pageHeader.classList.remove('page-header--nojs');
+pageHeader.classList.remove('page-header--opened-menu');
+pageHeader.classList.add('page-header--closed-menu');
 
-navToggle.addEventListener('click', function(){
-  if(mainNav.classList.contains('main-nav--closed')){
-    mainNav.classList.remove('main-nav--closed');
-    headerLogo.classList.remove('page-header__logo-wrapper--closed');
-    navToggle.classList.remove('page-header__toggle--closed');
-    navToggle.classList.add('page-header__toggle--opened');
+navToggle.addEventListener('click', function() {
+  if (pageHeader.classList.contains('page-header--closed-menu')) {
+    pageHeader.classList.remove('page-header--closed-menu');
+    pageHeader.classList.add('page-header--opened-menu');
+    navToggle.classList.add('hamburger-button--close-menu');
+    navToggle.classList.remove('hamburger-button--open-menu');
+  } else {
+    pageHeader.classList.add('page-header--closed-menu');
+    pageHeader.classList.remove('page-header--opened-menu');
+    navToggle.classList.remove('hamburger-button--close-menu');
+    navToggle.classList.add('hamburger-button--open-menu');
   }
-  else{
-    mainNav.classList.add('main-nav--closed');
-    headerLogo.classList.add('page-header__logo-wrapper--closed');
-    navToggle.classList.add('page-header__toggle--closed');
-    navToggle.classList.remove('page-header__toggle--opened');
-  }
-})
+});
